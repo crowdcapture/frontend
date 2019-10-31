8b8595f0bb4b47566ee269043bc5975c4aa56296
Pieter <gitkraken@pietervanmill.nl>
2019-10-31 09:40:41 +0100

    First work on the frontend. The home page, project page were build.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,52 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectOverviewComponent } from './project-overview/project-overview.component';
+import { UserOverviewComponent } from './user-overview/user-overview.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { SearchComponent } from './search/search.component';
+import { CreateProjectComponent } from './create-project/create-project.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'project/:projectId',
+    component: ProjectOverviewComponent
+  },
+  {
+    path: 'user/:userId',
+    component: UserOverviewComponent
+  },
+  {
+    path: 'my-projects',
+    component: MyProjectsComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  },
+  {
+    path: 'create',
+    component: CreateProjectComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectOverviewComponent } from './project-overview/project-overview.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { UserOverviewComponent } from './user-overview/user-overview.component';
+import { SearchComponent } from './search/search.component';
+import { UploadComponent } from './upload/upload.component';
+import { ValidateComponent } from './validate/validate.component';
+import { CreateProjectComponent } from './create-project/create-project.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    ProjectOverviewComponent,
+    MyProjectsComponent,
+    UserOverviewComponent,
+    SearchComponent,
+    UploadComponent,
+    ValidateComponent,
+    CreateProjectComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
