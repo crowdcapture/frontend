917f312ee70a71f0fe262bb30acd6b3c214e9842
Pieter <gitkraken@pietervanmill.nl>
2020-01-25 20:37:35 +0100

    My projects page finished.

--- src/app/pages/my-projects/my-projects.component.ts
+++ src/app/pages/my-projects/my-projects.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-my-projects',
   templateUrl: './my-projects.component.html',
   styleUrls: ['./my-projects.component.scss']
 })
-export class MyProjectsComponent implements OnInit {
+export class MyProjectsComponent implements OnInit, OnDestroy {
+  public projects: any;
+  private projectsSub: Subscription;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
+    this.projectsSub = this.httpClient.get(`${environment.url}/projects`).subscribe((result: any) => {
+      this.projects = result.projects;
+    });
   }
 
+  ngOnDestroy() {
+    this.projectsSub.unsubscribe();
+  }
 }
