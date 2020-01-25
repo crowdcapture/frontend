6f9dcef96304a94b9f7b701bbdd176bb2dd8fdb6
Pieter <gitkraken@pietervanmill.nl>
2020-01-25 19:54:59 +0100

    Make sure the user is actually logged out if  the token is not valid anymore.

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { UserService } from './user.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
     constructor(
-        private router: Router
+        private router: Router,
+        private userService: UserService
     ) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
@@ -17,6 +19,8 @@ export class AuthGuard implements CanActivate {
             // authorised so return true
             return true;
         }
+
+        this.userService.logoutUser();
 
         // not logged in so redirect to login page with the return url
         this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
