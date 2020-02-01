5ef08d0ce5fcd97beb24060aace9aece9221959f
Pieter <gitkraken@pietervanmill.nl>
2020-02-01 15:23:18 +0100

    First work on upload page.

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-upload',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent implements OnInit {
+  private uploadedFiles: File[] = [];
+  private projectId: string;
 
-  constructor() { }
+  public permission: boolean;
+  public owner: boolean;
+
+  constructor(
+    private httpClient: HttpClient,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.projectId = this.route.snapshot.paramMap.get('projectId');
   }
 
+  fileChange(element) {
+      this.uploadedFiles.push(element.target.files);
+      console.log(this.uploadedFiles);
+  }
+
+  upload() {
+      const formData = new FormData();
+
+      for (var i = 0; i < this.uploadedFiles.length; i++) {
+          formData.append('fileArray', this.uploadedFiles[i], this.uploadedFiles[i].name);
+      }
+
+      this.httpClient.post(`${environment.url}/project/${this.projectId}/upload`, formData).subscribe((response) => {
+          console.log('response received is ', response);
+      });
+  }
 }
