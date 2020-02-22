78d5b4e75e889797886af9a74fb03d9274bb6b6a
Pieter <gitkraken@pietervanmill.nl>
2020-02-22 16:43:48 +0100

    Update user page.

--- src/app/pages/user-overview/user-overview.component.ts
+++ src/app/pages/user-overview/user-overview.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-overview',
   templateUrl: './user-overview.component.html',
   styleUrls: ['./user-overview.component.scss']
 })
-export class UserOverviewComponent implements OnInit {
+export class UserOverviewComponent implements OnInit, OnDestroy {
+  public user: any;
+  public projects: any;
 
-  constructor() { }
+  private userSub: Subscription;
+  private id: string;
+
+  constructor(
+    private httpClient: HttpClient,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('userId');
+
+    this.userSub = this.httpClient.get(`${environment.url}/user/${this.id}`).subscribe((result: any) => {
+      this.user = result.user;
+      this.projects = result.projects;
+    });
   }
 
+  trackByFn(index, item) {
+    return item.id;
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
 }
