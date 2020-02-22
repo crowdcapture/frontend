7784744bd6479610dfce766ab4f076726b923e92
Pieter <gitkraken@pietervanmill.nl>
2020-02-22 18:57:02 +0100

    Updated the FAQ page.

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.scss']
 })
-export class FaqComponent implements OnInit {
+export class FaqComponent {
+  public questions: any;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
