b976c8a9963d8575a56ba69592c71f79e38fc05f
Pieter <gitkraken@pietervanmill.nl>
2021-03-06 12:58:11 +0100

    Added web monetization meta tag.

--- src/modules/shared/components/footer/footer.component.ts
+++ src/modules/shared/components/footer/footer.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+declare global {
+  interface Document {
+    monetization?: any;
+  }
+}
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  public monetization = false;
 
   constructor() { }
 
   ngOnInit() {
+    if (document.monetization && document.monetization.state === 'pending') {
+      this.monetization = true;
+    }
   }
-
 }
